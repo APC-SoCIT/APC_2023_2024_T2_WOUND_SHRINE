fb84e35c02d63499de5b703ebca9e20ff9641b9e
Bon Daggao <bsdaggao2@student.apc.edu.ph>
2023-12-07 11:14:43 +0800

    putting logo

--- frontend/src/components/HomePage.vue
+++ frontend/src/components/HomePage.vue
@@ -2,13 +2,18 @@
   <div>
     <body>
       <div class="menu">
-      <main>
-        <h1>CAMPER CAFE</h1>
-        <p>Est. 2020</p>
-        <section>
-          <h2>Coffee</h2>
-        </section>
-      </main>
+        <main>
+          <div class="logo-container">
+            <img src="/5Logo.png" alt="Logo" class="logo" />
+          </div>
+          <div class="info">
+            <h1>CAMPER CAFE</h1>
+            <p>Est. 2020</p>
+            <section>
+              <h2>Coffee</h2>
+            </section>
+          </div>
+        </main>
       </div>
     </body>
     <p>Welcome to our website!</p>
@@ -17,16 +22,31 @@
 
 <style>
  .menu {
+  text-align: center;
   border-radius: 25px;
   height: 20em;
-  text-align: center;
   width: 90%;
   background-color: #ffaa2b;
   margin: auto;
-  
-  }
+  margin-top: 30px;
+}
 
-  p {
+.logo-container {
+  margin-bottom: 20px; /* Adjust the margin as needed */
+}
+
+.logo {
+  width: 100px; /* Set the width of your logo */
+  height: auto; /* Maintain aspect ratio */
+}
+
+.info {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+p {
   text-align: center;
 }
 </style>
