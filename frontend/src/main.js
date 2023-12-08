039f8183de5a8bd8cbd2e4d6a6cba8cfb11970fa
Bon Daggao <bsdaggao2@student.apc.edu.ph>
2023-12-08 21:47:46 +0800

    new page

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,12 +3,14 @@ import App from './App.vue';
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from './components/HomePage.vue';
 import About from './components/AboutPage.vue';
+import Services from './components/ServicesComponent.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', component: Home },
-    { path: '/about', component: About },
+    { path: '/aboutPage', component: About },
+    { path: '/ServicesComponent', component: Services },
     // add your routes here
   ],
 });
