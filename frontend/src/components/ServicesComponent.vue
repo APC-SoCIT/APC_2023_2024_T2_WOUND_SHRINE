4401bf18b2f99808b732aeda7d236af60cd00bb3
Bon Daggao <bsdaggao2@student.apc.edu.ph>
2023-12-08 22:23:49 +0800

    trying carousel

--- frontend/src/components/ServicesComponent.vue
+++ frontend/src/components/ServicesComponent.vue
@@ -1,67 +1,69 @@
 <template>
-    <div>
-      <h1>Vue Slick Carousel Demo</h1>
-  
-      <div class="carousel-container">
-        <Carousel :options="carouselOptions">
-          <div v-for="item in carouselItems" :key="item.id" class="carousel-item">
-            <img :src="item.image" alt="Carousel Image" />
-            <div class="caption">
-              <h3>{{ item.title }}</h3>
-              <p>{{ item.description }}</p>
-            </div>
-          </div>
-        </Carousel>
+  <div>
+    <!-- Other components and content go here -->
+    <h1>CAMPER CAFE</h1>
+            <p>Est. 2020</p>
+    <h2>Contact Information</h2>
+    <!-- Vue Slick Carousel -->
+    <VueSlickCarousel :options="carouselOptions">
+      <!-- Carousel items go here -->
+      <div v-for="item in carouselItems" :key="item.id" class="carousel-item">
+        <img :src="item.image" alt="Carousel Image" />
+        <div class="caption">
+          <h3>{{ item.title }}</h3>
+          <p>{{ item.description }}</p>
+        </div>
       </div>
-    </div>
-  </template>
-  
-  <script>
-  import Carousel from 'vue-slick-carousel';
-  
-  export default {
-    components: {
-      Carousel,
-    },
-    data() {
-      return {
-        carouselOptions: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          autoplay: true,
-          arrows: true,
-        },
-        carouselItems: [
-          { id: 1, image: 'path/to/image1.jpg', title: 'Description 1', description: 'Lorem ipsum 1' },
-          { id: 2, image: 'path/to/image2.jpg', title: 'Description 2', description: 'Lorem ipsum 2' },
-          { id: 3, image: 'path/to/image3.jpg', title: 'Description 3', description: 'Lorem ipsum 3' },
-          // Add more items as needed
-        ],
-      };
-    },
-  };
-  </script>
-  
-  <style scoped>
-  .carousel-container {
-    margin-top: 30px;
-  }
-  
-  .carousel-item {
-    position: relative;
-  }
-  
-  .carousel-item img {
-    width: 100%;
-  }
-  
-  .caption {
-    position: absolute;
-    bottom: 0;
-    background-color: rgba(255, 255, 255, 0.8);
-    width: 100%;
-    padding: 10px;
-    text-align: center;
-  }
-  </style>
-  
+    </VueSlickCarousel>
+
+    <!-- Other components and content go here -->
+  </div>
+</template>
+
+<script>
+import VueSlickCarousel from 'vue-slick-carousel';
+
+export default {
+  components: {
+    VueSlickCarousel,
+  },
+
+  data() {
+    return {
+      carouselOptions: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        arrows: true,
+      },
+      carouselItems: [
+        { id: 1, image: 'path/to/image1.jpg', title: 'Description 1', description: 'Lorem ipsum 1' },
+        { id: 2, image: 'path/to/image2.jpg', title: 'Description 2', description: 'Lorem ipsum 2' },
+        { id: 3, image: 'path/to/image3.jpg', title: 'Description 3', description: 'Lorem ipsum 3' },
+        // Add more items as needed
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.carousel-item {
+  position: relative;
+}
+
+.carousel-item img {
+  width: 100%;
+}
+
+.caption {
+  position: absolute;
+  bottom: 0;
+  background-color: rgba(255, 255, 255, 0.8);
+  width: 100%;
+  padding: 10px;
+  text-align: center;
+}
+
+/* Add additional styles as needed */
+</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,13 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import { createRouter, createWebHistory } from 'vue-router';
+import VueSlickCarousel from 'vue-slick-carousel'
+import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
 import Home from './components/HomePage.vue';
 import About from './components/AboutPage.vue';
 import services from './components/ServicesComponent.vue';
 import Contact from './components/ContactInfo.vue';
+
 
 const router = createRouter({
   history: createWebHistory(),
@@ -20,5 +23,6 @@ const router = createRouter({
 const app = createApp(App);
 
 app.use(router);
+app.component('VueSlickCarousel', VueSlickCarousel)
 
 app.mount('#app');
