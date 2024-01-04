a9b54db9196777ed2c9ef8155ccfe0e50f72f7cb
Carlo231 <101620874+Carlo231@users.noreply.github.com>
2024-01-04 20:18:50 +0800

    Update config.php

--- church_backend/Church_Backend1/config.php
+++ church_backend/Church_Backend1/config.php
@@ -5,7 +5,7 @@ define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'demo');
  
-
+/* connection */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
 if($link === false){
