5113e489013ed0c87e40ce60c18aa644df469d6a
Carlo231 <101620874+Carlo231@users.noreply.github.com>
2024-01-05 14:11:47 +0800

    Update sql for website.sql

--- church_backend/Church_Backend1/sql for website.sql
+++ church_backend/Church_Backend1/sql for website.sql
@@ -1,34 +1,11 @@
--- phpMyAdmin SQL Dump
--- version 4.9.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Feb 22, 2022 at 01:42 AM
--- Server version: 10.4.8-MariaDB
--- PHP Version: 7.3.10
-
+-- sql
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `testdb05`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `myguests`
---
-
-CREATE TABLE `myguests` (
+CREATE TABLE `churchmembers` (
   `id` int(6) UNSIGNED NOT NULL,
   `firstname` varchar(30) NOT NULL,
   `lastname` varchar(30) NOT NULL,
@@ -36,37 +13,20 @@ CREATE TABLE `myguests` (
   `reg_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `myguests`
---
 
-INSERT INTO `myguests` (`id`, `firstname`, `lastname`, `email`, `reg_date`) VALUES
-(3, 'John', 'Doe', 'john@example.com', '2022-02-16 00:42:05'),
-(4, 'Jose', 'Quesada', 'jlquesada@apc.edu.ph', '2022-02-16 00:50:06'),
-(5, 'John', 'Doe', 'john@example.com', '2022-02-22 00:30:50'),
-(6, 'Pallas Dale', 'Fontiveros', 'jlquesada@apc.edu.ph', '2022-02-22 00:38:04');
+INSERT INTO `churchmembers` (`id`, `firstname`, `lastname`, `email`, `reg_date`) VALUES
+(3, 'Mario', 'Luigi', 'Mario@example.com', '2024-01-16 00:41:05'),
+(4, 'Gustavo', 'Dracula', 'Gustavo@apc.edu.ph', '2024-02-16 00:40:06'),
+(5, 'Gustavo', 'Biden', 'Gustav@example.com', '2024-03-22 00:30:20'),
+(6, 'Hokkaido', 'Manneh', 'Honkong@apc.edu.ph', '2024-04-22 00:35:01');
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `myguests`
---
-ALTER TABLE `myguests`
+ALTER TABLE `churchmembers`
   ADD PRIMARY KEY (`id`);
 
+REMENT for table `churchmembers`
 --
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `myguests`
---
-ALTER TABLE `myguests`
+ALTER TABLE `churchmembers`
   MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
