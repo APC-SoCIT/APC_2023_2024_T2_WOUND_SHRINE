2eab4b52530e7b7ae72876937b4a51d108908a49
Carlo231 <101620874+Carlo231@users.noreply.github.com>
2024-01-05 14:32:22 +0800

    Update passwordreset.php

--- church_backend/Church_Backend1/passwordreset.php
+++ church_backend/Church_Backend1/passwordreset.php
@@ -1,31 +1,26 @@
 <?php
-// Initialize the session
+// start
 session_start();
- 
-// Check if the user is logged in, otherwise redirect to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("location: login.php");
     exit;
 }
  
-// Include config file
 require_once "config.php";
  
-// Define variables and initialize with empty values
 $new_password = $confirm_password = "";
 $new_password_err = $confirm_password_err = "";
 if($_SERVER["REQUEST_METHOD"] == "POST"){
  
-  
     if(empty(trim($_POST["new_password"]))){
-        $new_password_err = "Please enter the new password.";     
+        $new_password_err = "Enter new password.";     
     } elseif(strlen(trim($_POST["new_password"])) < 6){
-        $new_password_err = "Password must have atleast 6 characters.";
+        $new_password_err = "6  or more characters needed";
     } else{
         $new_password = trim($_POST["new_password"]);
     } 
     if(empty(trim($_POST["confirm_password"]))){
-        $confirm_password_err = "Please confirm the password.";
+        $confirm_password_err = "Confirm password.";
     } else{
         $confirm_password = trim($_POST["confirm_password"]);
         if(empty($new_password_err) && ($new_password != $confirm_password)){
@@ -49,7 +44,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 header("location: login.php");
                 exit();
             } else{
-                echo "Oops! Something went wrong. Please try again later.";
+                echo "something is not right";
             }
 
             
