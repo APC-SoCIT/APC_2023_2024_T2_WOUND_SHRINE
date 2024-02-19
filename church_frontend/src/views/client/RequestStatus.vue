b72bebeec65e99b1eaeaad1e3325062d3e8b4b7d
Bon Daggao <bsdaggao2@student.apc.edu.ph>
2024-02-19 18:29:48 +0800

    Request status

--- church_frontend/src/views/client/RequestStatus.vue
+++ church_frontend/src/views/client/RequestStatus.vue
@@ -1,7 +1,35 @@
 <template>
-    <div>
-     <h1>title try</h1>
-   </div>
+  <div class="q-pa-md row items-start q-gutter-md">
+    <q-card class="my-card">
+      <q-card-section class="bg-primary text-white">
+        <div class="text-h6">Our Changing Planet</div>
+        <div class="text-subtitle2">by John Doe</div>
+      </q-card-section>
+
+      <q-separator />
+
+      <q-card-actions align="right">
+        <q-btn flat>Action 1</q-btn>
+        <q-btn flat>Action 2</q-btn>
+      </q-card-actions>
+    </q-card>
+  </div>
 </template>
-<script></script>
-<style></style>
+
+<script>
+export default {
+  setup () {
+    return {
+      lorem: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.my-card
+  width: 1000%
+  max-width: 1000px
+  margin: auto
+  margin-top: 20px
+</style>
