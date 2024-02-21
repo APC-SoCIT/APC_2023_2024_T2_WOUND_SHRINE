71d860cec20a37e911f2990c773223d78e9929c9
Bon Daggao <bsdaggao2@student.apc.edu.ph>
2024-02-22 01:07:51 +0800

    request status

--- church_frontend/src/views/client/RequestStatus.vue
+++ church_frontend/src/views/client/RequestStatus.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="q-pa-md row items-start q-gutter-md">
-    <q-card class="my-card">
-      <q-card-section class="bg-primary text-white">
-        <div class="text-h6">Our Changing Planet</div>
-        <div class="text-subtitle2">by John Doe</div>
-      </q-card-section>
-
-      <q-separator />
-
-      <q-card-actions align="right">
-        <q-btn flat>Action 1</q-btn>
-        <q-btn flat>Action 2</q-btn>
-      </q-card-actions>
-    </q-card>
+  <div>
+    <div v-for="index in 5" :key="index" class="q-pa-md row items-start q-gutter-md">
+      <q-card class="my-card">
+        <q-card-section class="bg-custom text-white"> <!-- Changed color to #ffaa2b -->
+          <div class="text-h6">Our Changing Planet</div>
+          <div class="text-subtitle2">by John Doe</div>
+        </q-card-section>
+  
+        <q-separator />
+  
+        <q-card-actions class="action-container" align="right">
+          <div class="action-word">Word {{ index }}</div>
+          <div>
+            <q-btn flat>edit</q-btn>
+            <q-btn flat>cancel</q-btn>
+          </div>
+        </q-card-actions>
+      </q-card>
+    </div>
   </div>
 </template>
 
@@ -32,4 +37,15 @@ export default {
   max-width: 1000px
   margin: auto
   margin-top: 20px
+
+.action-container
+  display: flex
+  justify-content: space-between
+  align-items: center
+
+.action-word
+  margin-right: 10px
+
+.bg-custom
+  background-color: #ffaa2b
 </style>
